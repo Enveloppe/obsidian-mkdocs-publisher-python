6804ccfeb96b8f9563749b846676f7f42f3bb87e
Mara <mara-li@outlook.fr>
2022-01-15 21:46:02 +0100

    docs(print): Repear msg delete all file

--- mkdocs_obsidian/__main__.py
+++ mkdocs_obsidian/__main__.py
@@ -136,6 +136,7 @@ def main():
     if not args.keep:
         info = check.delete_not_exist()
         if len(info) > 1:
+            info[0] = "- " + info[0]
             info_str = "\n- ".join(info)
             print(
                 f'[{datetime.now().strftime("%H:%M:%S")}] 🗑️ Delete from blog:\n{info_str}'
