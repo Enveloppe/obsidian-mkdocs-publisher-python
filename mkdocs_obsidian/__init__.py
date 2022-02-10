fd31d9b3f52181e9fb5cd66eabad2d0a3af03799
Mara <mara-li@outlook.fr>
2022-02-10 12:13:12 +0100

    style: Remove todo

--- mkdocs_obsidian/__init__.py
+++
@@ -1,8 +0,0 @@
-# Todo
-# Have the vault + mkdocs folder
-# Use the metadata from YAFPA aka : folder
-# Add css for img flags
-# Add customization in mkdocs yml (logo ; description)
-# Admonition code block (as always)
-# Add links in YML markdown
-# Create folder when category / folder doesn't exist ;)
