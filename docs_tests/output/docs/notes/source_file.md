275679ec9cf2e007bb37f8ce3a96e7c348d8f836
Mara <mara-li@outlook.fr>
2022-06-01 21:14:13 +0200

    test: Add files and test

--- docs_tests/output/docs/notes/source_file.md
+++ docs_tests/output/docs/notes/source_file.md
@@ -1,43 +1,50 @@
 ---
+cssclass: cards
 share: True
 ---
-- [ ] Doudou
-- [ ] Brosse à dent
-- [ ] Crème
-- [ ] Chargeurs
-- [ ] Ipad
+## What is this place?
+
+This is a sandbox vault in which you can test various functionality of Obsidian. 
+
+!!! Warning
+	Your changes will not be saved, so please don't take actual notes in this vault.
+
+!!! Note "Beta vault - contributions are welcome!"
+	This sandbox vault is in beta!
+	
+	If you spot a typo or a mistake, feel free to submit a pull request [here](https://github.com/obsidianmd/obsidian-docs/tree/master/Sandbox).
+
+
+Different than most note-taking apps out there, an Obsidian vault is not a notebook on our server or a bundle in a special format on your computer.
+
+In Obsidian, a vault is a plain folder on your computer. By plain, I mean it's just like a folder you created and put some files in. You can open it with your system explorer or Finder, zip it up, make a copy, or move it around.
+
+!!! SUMMARY "The Point™"
+	The point is that you own this folder just like the other folder of camping photos that you created yourself.
+	
+	It’s supposed to be a good thing because you get both **data ownership and privacy** this way. Just keep in mind [[#^0f681f|with great power comes great responsibility]].
+
+No need to worry about loading your content from our servers. You know, servers can get hacked or be offline or be slow when you need it most.
+
+No need to worry about exporting or converting if you move way from Obsidian either. The vault is just a folder that sits here—Obsidian is not going to delete any of your vault folders just because you uninstall Obsidian.
+
+Finally, please understand that with great power comes great responsibility. Now that you’re in charge of your data, it’s also your job to back it up and keep it safe. ^0f681f
+
+??? Note "A word on moving your vault folder"
+	Technically you can freely move your vault folder around just like how you move any other folder. But if you do, Obsidian will be confused, so make sure to open it again at the new location with Obsidian after you moved it.
+
 ---
-Les étoiles brillent joliment.
-Qq 
-********************coucou**{: }**{: }**{: }**{: }**{: }**{: }**{: }**{: }**{: }**{: #left}  
 
-!!! NOTE
-	notes
+If you’re convinced Obsidian is worth a try for you, it’s time to get started
 
-**tags1**{: **tags1**{: **tags1**{: **tags1**{: **tags1**{: **tags1**{: **tags1**{: **tags1**{: **tags1**{: **tags1**{: #tags1 .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- **tags2**{: **tags2**{: **tags2**{: **tags2**{: **tags2**{: **tags2**{: **tags2**{: **tags2**{: **tags2**{: **tags2**{: #tags2 .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
- .hash}  
+# Other test
+
+- **tags**{: #tags .hash}  
 
 
 ```md
-do not convert this
-> [!Notes]
-> notes
+#test
+don't touch this
+> [!warning]
+> no !!!
 ```
--- mkdocs_obsidian/test_sample.py
+++ mkdocs_obsidian/test_sample.py
@@ -1,31 +1,77 @@
 import unittest
 from pathlib import Path
+
+import yaml
+
 from mkdocs_obsidian.common import convert_one as one, convert_all as all, config as cfg
 
 
 class MyTestCase(unittest.TestCase):
     def test_minimal(self):
         print("Testing Minimal configuration")
-        source_path = Path("../docs_tests/input/source_file.md").resolve()
-        configuration = cfg.open_value("0", actions="minimal", test=True)
+        source_path = Path("./docs_tests/input/source_file.md").resolve()
+        configuration = cfg.open_value(actions=True, configuration_name='minimal_test')
         one.overwrite_file(str(source_path), configuration, True)
 
     def test_multiple_notes(self):
         print("testing multiple note without share state")
-        configuration = cfg.open_value("0", test=True)
+        configuration = cfg.open_value("test")
         all.convert_all(configuration, False, False, 0, 0, 1)
 
-    def one_file(self):
+    def test_one_file(self):
         print("Testing one file with all configuration")
-        configuration = cfg.open_value("0", test=True)
-        source_path = Path("../docs_tests/input/source_file.md").resolve()
+        configuration = cfg.open_value("test")
+        source_path = Path("./docs_tests/input/source_file.md").resolve()
         one.convert_one(source_path, configuration, False, 1)
 
-    def multiple_share(self):
+    def test_multiple_share(self):
         print("testing multiple note with share state")
-        configuration = cfg.open_value("0", test=True)
+        configuration = cfg.open_value("test")
         all.convert_all(configuration, False, False, 0, 0, 0)
 
+    def test_convert_big_config(self):
+        print('convert an old configuration')
+        basedir = cfg.get_Obs2mk_dir('test_config', False)
+        config_test = Path(basedir, ".test_config")
+        with open(config_test, 'w', encoding="utf-8") as f:
+            f.write(f'''
+                vault={Path(basedir, 'input')}
+                blog_path={Path(basedir, 'output')}
+                blog=https://www.mara-li.fr/
+                share=share
+                index_key=(i)
+                default_blog=notes
+                category_key=category
+            ''')
+        cfg.checking_old_config('test_config', config_test, basedir)
+        with open(Path(basedir, 'configuration.yml'), 'r', encoding='utf-8') as config:
+            new_config = yaml.safe_load(config)
+        new_config = new_config['test_config']
+        attended_result = '''
+        configuration:
+            input: D:\\Users\Lili\Documents\GitHub\mkdocs obsidian publish\mkdocs_obsidian_publish\docs_tests\output\input
+            output: D:\\Users\Lili\Documents\GitHub\mkdocs obsidian publish\mkdocs_obsidian_publish\docs_tests\output\output
+        frontmatter:
+            category:
+                default value: notes
+                key: category
+            index: (i)
+            share: share
+        weblink: https://www.mara-li.fr/
+        '''
+        attended_result_to_yaml = yaml.safe_load(attended_result)
+        print(attended_result_to_yaml == new_config)
 
+    def test_convert_mini_config(self):
+        print('convert an old configuration')
+        basedir = cfg.get_Obs2mk_dir('test_mini_config', False)
+        config_test = Path(basedir, ".test_mini_config")
+        with open(config_test, 'w', encoding="utf-8") as f:
+            f.write(f'''
+            index_key=(i)
+            default_blog=notes
+            category_key=category
+            ''')
+        cfg.checking_old_config('test_mini_config', config_test, basedir)
 if __name__ == "__main__":
     unittest.main()
